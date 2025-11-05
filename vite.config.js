39fbd37fd0b3de38df7f5cbb383a79920b4e0c2d
MbazzaTZ <davidmbazza@gmail.com>
2025-11-05 13:24:09 +0300

    Added vercel.json for full PWA + offline support

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ export default defineConfig({
   publicDir: "public",
   build: {
     outDir: "dist",
-    emptyOutDir: true,
+    rollupOptions: {
+      input: "./index.html",
+    },
   },
   server: {
     port: 5173,
+    open: true,
   },
 });
